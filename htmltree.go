c3de580672c6d43646f567b2ea81449e3de93f94
Frank Storbeck <frank@foef.nl>
2018-05-28 13:03:35 +0200

    added copyright notices.

--- htmltree.go
+++ htmltree.go
@@ -6,7 +6,30 @@
 //
 // definitions and functions for constructing an HTML tree.
 //
-// © 2018 Frank Storbeck
+//
+// Copyright © 2018 Frank Storbeck. All rights reserved.
+// Code licensed under the BSD License:
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 package main
 
@@ -66,7 +89,7 @@ func (ht *HTMLTree) BlockQuote(s string) error {
 	return nil
 }
 
-// Traverse Traverses a slice of siblings. For each string found the function
+// Traverse traverses a slice of siblings. For each string found the function
 // 'f' is called with this string as an argument. This sibling is then replaced
 // by the result of 'f'. When the sibling is a pointer to a branch Traverse is
 // called with a slice of all siblings in this branch as the argument.
@@ -434,9 +457,10 @@ func (ht *HTMLTree) ListItem(s string, indnt, nEnd int) error {
 }
 
 // ListParent set the current branch to the 'n'-th parent that is a 'ul{..}'
+// or 'ol{...}'
 func (ht *HTMLTree) ListParent(n int) error {
 	for n > 0 {
-		if ht.br.ID == "ul" {
+		if ht.br.ID == "ul" || ht.br.ID == "ol" {
 			n--
 		}
 		err := ht.TryParent(1)
